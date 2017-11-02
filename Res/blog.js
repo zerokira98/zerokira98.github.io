b25e26873e5e059e0727832e4c6868f4d67013be
zerokira98 <31804242+zerokira98@users.noreply.github.com>
2017-11-02 21:39:48 +0700

    awaw

--- Res/blog.js
+++ Res/blog.js
@@ -1,5 +1,5 @@
 var link = "http://www.facebook.com/messavb";
-var fbook = "<li><a href=".link.">Facebook</a></li>";
+var fbook = "<li><a href="+link+">Facebook</a></li>";
 
 $("p").css("background-color", "yellow");
 $("#templatemo_menu ul").append(fbook);
