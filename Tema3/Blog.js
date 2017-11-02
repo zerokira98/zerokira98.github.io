d608eb3820124ec255f1b8b468b9de438f6aee51
zerokira98 <31804242+zerokira98@users.noreply.github.com>
2017-11-03 06:23:17 +0700

    awda

--- Tema3/Blog.js
+++ Tema3/Blog.js
@@ -8,8 +8,9 @@ var options = [
     ];
     Materialize.scrollFire(options);
 $("#templatemo_menu_section ul").append(fbook);
-$("#templatemo_menu_section").addClass('card-panel red darken-2');
-$("#templatemo_menu_section ul li a").addClass('white-text waves-effect waves-light btn-flat red');
+$("#templatemo_menu_section ul").attr('class','right').attr('id','nav-mobile');
+$("#templatemo_menu_section").addClass('card-panel red darken-2').attr('id','nav-wrapper');
+//$("#templatemo_menu_section ul li a").addClass('white-text waves-effect waves-light btn-flat red');
 $(".templatemo_right_section_box").addClass('card-panel');
 $(".templatemo_left_section_box_top").addClass('card-panel red darken-1 white-text');
 $(".templatemo_left_section_box_top a").addClass('white-text text-darken-2');
