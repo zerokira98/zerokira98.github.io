9d77482bf6d94a731ac4de6175e36c2802985712
zerokira98 <31804242+zerokira98@users.noreply.github.com>
2017-11-02 22:32:17 +0700

    awaw

--- Tema3/Blog.js
+++ Tema3/Blog.js
@@ -4,4 +4,4 @@ var fbook = "<li><a href="+link+">Facebook</a></li>";
 $("#templatemo_menu_section ul").append(fbook);
 $("#templatemo_menu_section").addClass('card-panel red darken-1');
 $(".templatemo_left_section_box_top").addClass('card-panel red darken-1');
-$("a").addClass('white-text text-darken-2');
+$(".templatemo_left_section_box_top a").addClass('white-text text-darken-2');
