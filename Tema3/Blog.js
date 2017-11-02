b0e993b750f816f76beec9314b7b33796d3a38c8
zerokira98 <31804242+zerokira98@users.noreply.github.com>
2017-11-03 00:30:28 +0700

    awdas

--- Tema3/Blog.js
+++ Tema3/Blog.js
@@ -1,6 +1,11 @@
 var link = "http://www.facebook.com/messavb";
 var fbook = "<li><a href="+link+">Facebook</a></li>";
-
+var options = [
+      {selector: '#templatemo_footer', offset: 50, callback: function(el) {
+        Materialize.toast("End of the page...", 1500 );
+      } }
+    ];
+    Materialize.scrollFire(options);
 $("#templatemo_menu_section ul").append(fbook);
 $("#templatemo_menu_section").addClass('card-panel red darken-2');
 $("#templatemo_menu_section ul li a").addClass('white-text waves-effect waves-light btn-flat red');
@@ -8,3 +13,4 @@ $(".templatemo_right_section_box").addClass('card-panel');
 $(".templatemo_left_section_box_top").addClass('card-panel red darken-1 white-text');
 $(".templatemo_left_section_box_top a").addClass('white-text text-darken-2');
 $(".templatemo_left_section_box_body").addClass('card-panel');
+$(".templatemo_middle_section_box").addClass('card-panel');
