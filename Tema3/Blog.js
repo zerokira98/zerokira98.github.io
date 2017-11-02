d370b1bf684af6c869b0e44a849bf99d977cb0aa
zerokira98 <31804242+zerokira98@users.noreply.github.com>
2017-11-02 22:14:21 +0700

    waw

--- Tema3/Blog.js
+++ Tema3/Blog.js
@@ -2,3 +2,4 @@ var link = "http://www.facebook.com/messavb";
 var fbook = "<li><a href="+link+">Facebook</a></li>";
 
 $("#templatemo_menu_section ul").append(fbook);
+$("#templatemo_menu_section").addClass('card-panel teal lighten-2');
