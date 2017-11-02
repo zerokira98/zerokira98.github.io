4ffbdb61adf0750052e36e98d9c25b758a44e6e0
zerokira98 <31804242+zerokira98@users.noreply.github.com>
2017-11-03 06:48:52 +0700

    awdas

--- Tema3/Blog.js
+++ Tema3/Blog.js
@@ -12,7 +12,7 @@ $("#templatemo_menu_section ul").attr('class','right');
 $("#templatemo_menu_section").addClass('card-panel red ');
 $("#templatemo_menu_section ul li a").addClass('white-text waves-effect waves-light btn-flat red');
 $(".templatemo_right_section_box").addClass('card-panel');
-$(".templatemo_left_section_box_top").addClass('card-panel red darken-1 white-text');
+$(".templatemo_left_section_box_top").addClass('card-panel red darken-1 white-text waves-effect waves-light');
 $(".templatemo_left_section_box_top a").addClass('white-text text-darken-2');
 $(".templatemo_left_section_box_body").addClass('card-panel');
 $(".templatemo_middle_section_box").addClass('card-panel');
