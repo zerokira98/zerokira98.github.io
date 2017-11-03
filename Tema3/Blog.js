a57787bec7d01808b5719ead2c1ad9eab73c90b1
zerokira98 <31804242+zerokira98@users.noreply.github.com>
2017-11-03 09:48:38 +0700

    asd

--- Tema3/Blog.js
+++ Tema3/Blog.js
@@ -9,12 +9,13 @@ var options = [
     Materialize.scrollFire(options);
     $(document).ready(function(){
       $('.parallax').parallax();
+      $(".templatemo_right_section_box").addClass('card-panel');
     });
 
 $("#templatemo_menu_section ul").append(fbook);
 $("#templatemo_menu_section").addClass('card-panel');
 $("#templatemo_menu_section ul li a").addClass('white-text waves-effect waves-light btn-flat');
-$(".templatemo_right_section_box").addClass('card-panel');
+
 $(".templatemo_left_section_box_top").addClass('card-panel red darken-1 white-text waves-effect waves-light');
 $(".templatemo_left_section_box_top a").addClass('white-text text-darken-2');
 $(".templatemo_left_section_box_body").addClass('card-panel');
