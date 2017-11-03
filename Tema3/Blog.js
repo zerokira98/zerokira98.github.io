7b2216f7198e9111a885ee8ed5a18e351dccef0f
zerokira98 <31804242+zerokira98@users.noreply.github.com>
2017-11-03 08:05:10 +0700

    dwasdas

--- Tema3/Blog.js
+++ Tema3/Blog.js
@@ -8,8 +8,7 @@ var options = [
     ];
     Materialize.scrollFire(options);
 $("#templatemo_menu_section ul").append(fbook);
-$("#templatemo_menu_section ul").attr('class','right');
-$("#templatemo_menu_section").addClass('card-panel red ');
+$("#templatemo_menu_section").addClass('card-panel');
 $("#templatemo_menu_section ul li a").addClass('white-text waves-effect waves-light btn-flat');
 $(".templatemo_right_section_box").addClass('card-panel');
 $(".templatemo_left_section_box_top").addClass('card-panel red darken-1 white-text waves-effect waves-light');
