740ae864b3c8d9abfd6e63a946aea38b5f7fc88e
zerokira98 <31804242+zerokira98@users.noreply.github.com>
2017-11-03 10:51:50 +0700

    ugyjbujh

--- Tema3/Blog.js
+++ Tema3/Blog.js
@@ -1,9 +1,11 @@
 var link = "http://www.facebook.com/messavb";
 var fbook = "<li><a href="+link+">Facebook</a></li>";
 var parralaks = "<div class='parallax-container'><div class='parallax'><img src='https://zerokira98.github.io/Res/vlcsnap-2017-05-29-17h53m02s337.png'></div></div>"
+
+$("#templatemo_background_section_top").clone().appendTo(".parallax-container");
 var options = [
       {selector: '#templatemo_footer', offset: 10, callback: function(el) {
-        Materialize.toast("End of the page...", 2500 );
+        Materialize.toast("End of the page...", 1500 );
       } }
     ];
     Materialize.scrollFire(options);
@@ -11,7 +13,6 @@ var options = [
       $('.parallax').parallax();
       $(".templatemo_right_section_box").addClass('card-panel');
     });
-
 $("#templatemo_menu_section ul").append(fbook);
 $("#templatemo_menu_section").addClass('card-panel');
 $("#templatemo_menu_section ul li a").addClass('white-text waves-effect waves-light btn-flat');
@@ -20,4 +21,4 @@ $(".templatemo_left_section_box_top").addClass('card-panel red darken-1 white-te
 $(".templatemo_left_section_box_top a").addClass('white-text text-darken-2');
 $(".templatemo_left_section_box_body").addClass('card-panel');
 $(".templatemo_middle_section_box").addClass('card-panel');
-$("#templatemo_background_section_top").after(parralaks);
+$("body").prepend(parralaks);
