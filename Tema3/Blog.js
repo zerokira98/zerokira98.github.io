c19e083e2142af080a2b0c4a146ca9730ad2b3d3
zerokira98 <31804242+zerokira98@users.noreply.github.com>
2017-11-04 08:15:13 +0700

    asdas

--- Tema3/Blog.js
+++ Tema3/Blog.js
@@ -18,10 +18,10 @@ var options = [
 $("#templatemo_menu_section ul").append(fbook);
 $("#templatemo_menu_section").addClass('card-panel');
 $("#templatemo_menu_section ul li a").addClass('white-text waves-effect waves-light btn-flat');
-$(".templatemo_left_section_box_top").addClass('card-panel red darken-1 white-text waves-effect waves-light');
+$(".templatemo_left_section_box_top").addClass('card-panel red white-text waves-effect waves-light');
 $(".templatemo_left_section_box_top a").addClass('white-text text-darken-2');
 $(".templatemo_left_section_box_body").addClass('card-panel');
 $(".templatemo_middle_section_box").addClass('card-panel');
-$("#templatemo_content_area").addClass('row');
+$("#templatemo_content_area").addClass('row card-panel');
 $("#templatemo_left_section").addClass('col s12 m9 l9').attr('id','left_section');
 $("#templatemo_right_section").addClass('col s12 m3 l3').attr('id','right_section');
