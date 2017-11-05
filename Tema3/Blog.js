388cf7d5eff4e667de503eb8e0d113d3a445fe19
zerokira98 <31804242+zerokira98@users.noreply.github.com>
2017-11-05 15:51:43 +0700

    asdasd

--- Tema3/Blog.js
+++ Tema3/Blog.js
@@ -1,3 +1,4 @@
+
 //------------------code
 
 var parralaks = "<div class='parallax-container'><div class='parallax'><img src="+BackgroundTop+"></div></div>"
@@ -14,9 +15,9 @@ $("#templatemo_menu_section ul").append(menu1);
 var menu2 = "<li><a href="+link2+">"+NamaMenu2+"</a></li>";
 $("#templatemo_menu_section ul").append(menu2);
 //----End Add Menu
-$("#templatemo_menu_section").addClass('card-panel');
-$("#templatemo_menu_section ul li a").addClass('white-text waves-effect waves-light btn-flat');
-$(".templatemo_left_section_box_top").addClass('card-panel red white-text waves-effect waves-light');
+$("#templatemo_menu_section").addClass('card-panel '+panelcolor);
+$("#templatemo_menu_section ul li a").addClass('white-text waves-effect waves-light btn-flat'+panelcolor);
+$(".templatemo_left_section_box_top").addClass('card-panel white-text waves-effect waves-light'+panelcolor);
 $(".templatemo_left_section_box_top a").addClass('white-text text-darken-2');
 $(".templatemo_left_section_box_body").addClass('card-panel');
 $(".templatemo_middle_section_box").addClass('card-panel');
