4b136f4c3f3f0b323499dcf15e719b833c01d229
zerokira98 <31804242+zerokira98@users.noreply.github.com>
2018-02-07 21:33:49 +0700

    awawaw

--- Tema3/Blog.js
+++ Tema3/Blog.js
@@ -12,12 +12,14 @@ $("#templatemo_background_section_top").clone().appendTo(".parallax-container").
       $(".gist").width(gitwidth);
       $('.modal').modal();
     });
+
 //----Add Menu
 var menu1 = "<li><a href="+link1+">"+NamaMenu1+"</a></li>";
 var menu2 = "<li><a href="+link2+">"+NamaMenu2+"</a></li>";
 $("#templatemo_menu_section ul").append(menu1).append(menu2).append("<li><a class='white-text modal-trigger' href='#modal1'><i class='material-icons'>menu</i></a></li>");
 //----End Add Menu
 
+//-----etc
 $("#templatemo_menu_section").addClass('card-panel '+panelcolor);
 $("#templatemo_menu_section ul li a").addClass('white-text waves-effect waves-light btn-flat visimenu');
 $(".modal-trigger").removeClass('visimenu');
@@ -29,10 +31,12 @@ $("#templatemo_content_area").addClass('row card-panel');
 $("#templatemo_left_section").addClass('col s12 m12 l9').attr('id','left_section');
 $("#templatemo_right_section").addClass('col s12 m12 l3').attr('id','right_section');
 $("#header .templatemo_container").attr('class', 'menu_container');
+
 //---- Add Modal
 var modalmenu1 = "<li><a href='index.html'>Home</a></li>";
 var modalmenu2 = "<li><a href='profil.html'>Profil</a></li>";
 var modalmenu3 = "<li><a href='http://web.unair.ac.id/login.html' >Login</a></li>";
-var modalpanel = "<div id='modal1' class='modal bottom-sheet'><div class='modal-content'><h4>Menu</h4>"+modalmenu1+modalmenu2+modalmenu3+menu1+menu2+"</div></div>";
+var modalpanel = "<div id='modal1' class='modal bottom-sheet'><div class='modal-content'><h4>Menu</h4><ul>"+modalmenu1+modalmenu2+modalmenu3+menu1+menu2+"</ul></div></div>";
 $('body').append(modalpanel);
-$('.modal-content li').addClass('btn-flat grey lighten-4 black-text');
+$('.modal-content ul li a').addClass('black-text');
+$('.modal-content ul li').addClass('btn-flat');
